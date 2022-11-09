e5f6441771fc0bd3f025e32ecf177d568b1219b9
melashu <meshu102@outlook.com>
2022-11-09 16:51:19 +0300

    Person inherit Namebale interface

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,20 @@
-class Person
+require_relative './nameable'
+require_relative './capitalize_decorator'
+require_relative './trimmer_decorator'
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
-
+ 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
+    super()
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
@@ -23,3 +31,12 @@ class Person
     @age >= 18
   end
 end
+
+person = Person.new(22, 'meshu')
+puts person.correct_name
+capitalize_decorator = CapitalizeDecorator.new(person)
+
+puts capitalize_decorator.correct_name
+
+trimmer_decorator = TrimmerDecorator.new(capitalize_decorator)
+puts trimmer_decorator.correct_name
