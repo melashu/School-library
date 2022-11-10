e4cefb8d5fda8dfb27c7797420d95f53ed93bf97
melashu <meshu102@outlook.com>
2022-11-10 15:47:58 +0300

    add rentals variable

--- lib/person.rb
+++ lib/person.rb
@@ -3,13 +3,14 @@ require_relative './capitalize_decorator'
 require_relative './trimmer_decorator'
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
 
